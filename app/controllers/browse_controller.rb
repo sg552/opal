20f7c62e963126cee21c3980e0c11bc99951019c
Siwei Shen <sg552sg552@gmail.com>
2011-12-29 17:48:27 +0800

    removed redundant 'tail blank' and some puts statement

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -4,15 +4,18 @@ class BrowseController < ApplicationController
   def index
     @categories = Category.find(:all, :select => "name, id", :limit => 1000, :order => "name asc")
     @setting[:homepage_type] = Setting.get_setting("homepage_type")
-    
+
     if @setting[:homepage_type] == "new_items"
-      @items = Item.paginate :page => params[:page], :per_page => @setting[:items_per_page].to_i, :order => Item.sort_order(params[:sort]), :conditions => [" is_approved = '1' and is_public = '1'"]     
-    end  
+      @items = Item.paginate :page => params[:page],
+        :per_page => @setting[:items_per_page].to_i,
+        :order => Item.sort_order(params[:sort]),
+        :conditions => [" is_approved = '1' and is_public = '1'"]
+    end
   end
-  
-  def lost # they're lost   
+
+  def lost # they're lost
   end
-    
+
   def user
     redirect_to :action => "show", :controller => "users", :id => params[:id]
   end
@@ -22,6 +25,6 @@ class BrowseController < ApplicationController
     @fruit="banana"
     render :text => "hi, #{@fruit}"
   end
-  
-  private  
+
+  private
 end
