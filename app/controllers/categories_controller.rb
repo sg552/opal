c817d4e8873ee87b286a1228c2c10803d635fc69
Siwei Shen <sg552sg552@gmail.com>
2011-12-31 19:37:50 +0800

    solved mysql compatible problem, for Category:   1. categories could be created/edited correctly   2. re-organized the format   3. re-implement method: Category.get_parent_categories   4. removed redundant var from view page.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,28 +1,28 @@
 class CategoriesController < ApplicationController
-  before_filter :authenticate_admin # make sure logged in user is an admin   
+  before_filter :authenticate_admin # make sure logged in user is an admin
   before_filter :enable_admin_menu
-    
-  def index 
+
+  def index
     @setting[:meta_title] << Category.model_name.human(:count => :other)
-    @categories = Category.find(:all, :conditions =>["category_id = 0"], :order => "name ASC")
+    @categories = Category.get_parent_categories
   end
-  
+
   def edit
-   if params[:id] 
+   if params[:id]
      @category = Category.find(params[:id])
-   end     
+   end
   end
-  
-  def new 
+
+  def new
    @category = Category.new
-   if params[:id] 
+   if params[:id]
      @category.category_id = Category.find(params[:id]).id
-   end   
+   end
   end
-  
+
   def update
-    category = Category.find(params[:id])    
-    if params[:category][:category_id].to_i != category.id # trying to select self as parent category 
+    category = Category.find(params[:id])
+    if params[:category][:category_id].to_i != category.id # trying to select self as parent category
       if category.update_attributes(params[:category])
         log(:target => category,  :log_type => "update")
         flash[:success] = t("notice.save_success")
@@ -31,12 +31,12 @@ class CategoriesController < ApplicationController
       end
     else
       flash[:failure] = t("notice.association_loop_failure", :item => Category.model_name.human)
-    end      
+    end
     redirect_to :action => "index"
   end
-  
+
   def create # creates a new Feature, not a Feature Value
-    #category = Category.find(params[:id])   
+    #category = Category.find(params[:id])
     category = Category.new(params[:category])
     if category.save
       log(:target => category,  :log_type => "create")
@@ -47,14 +47,14 @@ class CategoriesController < ApplicationController
         flash[:notice] << "<b>#{key}</b>...#{value}</font><br>" #print out any errors!
        end
       flash[:notice] << ""
-      
-      
+
+
     end
     redirect_to :action => "index"
   end
- 
-  def delete # deletes feature 
-    category = Category.find(params[:id])    
+
+  def delete # deletes feature
+    category = Category.find(params[:id])
     if category.destroy
       log(:target => category,  :log_type => "destroy")
       flash[:success] = t("notice.item_delete_success", :item => Category.model_name.human)
