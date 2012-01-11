bda0a4daa2585999063445b0b91b3307989152b5
Siwei Shen <sg552sg552@gmail.com>
2012-01-12 00:29:31 +0800

    added TODO: use english instead of translation as action name

--- config/routes.rb
+++ config/routes.rb
@@ -18,10 +18,12 @@ Rails.application.routes.draw do
   match 'admin', :controller => "admin", :action => "index"
   #match "#{params[:controller]}", :controller => params[:record].class.name, :as => :record_path
 
+
+  # TODO use english name only, but not the translation as the action name
   if (Setting.get_setting('locale').to_s == 'en')
-    match "/#{Item.model_name.human(:count => :other).downcase}(/:action(/:id(.:format)))", :controller => "items" # use plural item name in url for anything in the items controller 
+    match "/#{Item.model_name.human(:count => :other).downcase}(/:action(/:id(.:format)))", :controller => "items" # use plural item name in url for anything in the items controller
   elsif (Setting.get_setting('locale').to_s == 'ru')
-    match "/#{Russian.translit(Item.model_name.human(:count => :other)).downcase}(/:action(/:id(.:format))", :controller => "items" # Russian variant uses transliteration to avoid encoding troubles 
+    match "/#{Russian.translit(Item.model_name.human(:count => :other)).downcase}(/:action(/:id(.:format))", :controller => "items" # Russian variant uses transliteration to avoid encoding troubles
   end
 
   match '/blog/:year(/:month(/:day))',
@@ -31,21 +33,21 @@ Rails.application.routes.draw do
                :constraints  => {:year => /\d{4}/, :month => /\d{1,2}/,:day => /\d{1,2}/},
                :as => :blog_archive
 
- 
 
-   
+
+
   # Simple Captcha
   match '/simple_captcha/:id', :to => 'simple_captcha#show', :as => :simple_captcha
 
 
-  # Resources  
+  # Resources
   resources :user_sessions
   # User Authentication
   match 'login', :controller => "user_sessions", :action => "new"
   match 'logout', :controller => "user_sessions", :action => "destroy"
 
-  
-  resources :users do 
+
+  resources :users do
     get "verification_required", :on => :collection
   end
 
@@ -56,23 +58,23 @@ Rails.application.routes.draw do
   match '/auth/:provider/callback' => 'authentications#create'
   match '/auth/failure' => 'authentications#failure'
 
-  
-  
-  resources :plugin_videos do 
-    get :delete, :on => :collection 
+
+
+  resources :plugin_videos do
+    get :delete, :on => :collection
   end
-  
+
   resources :logs do
     get "for_me", :on => :collection
     get "for_item", :on => :collection
-    get "for_user", :on => :collection            
+    get "for_user", :on => :collection
   end
-  
+
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   match ':controller(/:action(/:id(.:format)))'
 
-  
+
 end
 
 
