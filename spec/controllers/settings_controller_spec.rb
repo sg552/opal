5b4525618115b26f3c33511231d66a9b50f80950
Siwei Shen <sg552sg552@gmail.com>
2012-01-16 17:59:21 +0800

    fixed fail unit tests for settings_controlelr

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -1,89 +1,90 @@
 require 'spec_helper'
 
-describe SettingsController do  
+describe SettingsController do
   render_views
-  
+
   context "as admin" do
     before(:each) do
       login_admin
-    end 
+    end
 
     describe :index do
       it "GET index returns 200" do
         get :index
         response.code.should eq("200")
-      end          
+      end
     end
 
     describe :new_change_logo do
       it "returns 200" do
         get :new_change_logo
         response.code.should eq("200")
-      end    
+      end
     end
-    
-		describe "handling the site logo" do
-			before(:each) do
-				@image_path = Rails.root.to_s + "/public/themes/fracture/images/logo.png" # location of main logo
-				if File.file?(@image_path) 
-					@image_saved = Rails.root.to_s + "/public/themes/fracture/images/logo-saved.png" # temp save location for the original logo
-					FileUtils.mv(@image_path, @image_saved)
-				end				
-			end
 
-			after(:each) do
-				FileUtils.rm(@image_path) if File.file?(@image_path)
-				FileUtils.mv(@image_saved, @image_path) unless @image_saved.nil?
-			end
-			
-			describe :change_logo do 
-				it "uploads a remote logo" do
-					post(:change_logo, { :source => :remote, :url => "https://github.com/hulihanapplications/Opal/raw/dev/public/themes/fracture/screenshot.png"})
-					flash[:success].should_not == nil
-					File.file?(@image_path).should == true
-					response.should redirect_to(:action => :index, :controller => :settings)
-				end
+    describe "handling the site logo" do
+      before(:each) do
+        @image_path = Rails.root.to_s + "/public/themes/fracture/images/logo.png" # location of main logo
+        if File.file?(@image_path)
+          @image_saved = Rails.root.to_s + "/public/themes/fracture/images/logo-saved.png" # temp save location for the original logo
+          FileUtils.mv(@image_path, @image_saved)
+        end
+      end
 
-				it "uploads a local logo" do
-          uploaded_file = fixture_file_upload(Rails.root.join('spec/fixtures/images/example.png'), "image/png")       
-          post(:change_logo, {:source => :local, :file => uploaded_file})
-					File.file?(@image_path).should == true
-					response.should redirect_to(:action => :index, :controller => :settings)
-				end
-				
-				it "fails uploading a non-image logo" do
-				  uploaded_file = fixture_file_upload(Rails.root.join('spec/fixtures/videos/example.flv'), "video/x-flv")       
-					post(:change_logo, { :source => :local, :file => uploaded_file })
-					flash[:failure].should_not == nil
-					File.file?(@image_path).should == false
-					response.should redirect_to(:action => :index, :controller => :settings)
-				end
-			end
+      after(:each) do
+        FileUtils.rm(@image_path) if File.file?(@image_path)
+        FileUtils.mv(@image_saved, @image_path) unless @image_saved.nil?
+      end
 
-			describe :delete_logo do 
-				it "really erases the logo" do
-					FileUtils.touch(@image_path)
-					get(:delete_logo)
-					flash[:success].should_not == nil
-					File.file?(@image_path).should == false
-					response.should redirect_to(:action => :index, :controller => :settings, :anchor => :Logo)
-				end
-			end  
+      describe :change_logo do
+        it "uploads a remote logo" do
+          post(:change_logo, { :source => :remote, :url => "https://github.com/hulihanapplications/Opal/raw/dev/public/themes/fracture/screenshot.png"})
+          flash[:success].should_not == nil
+          File.file?(@image_path).should == true
+          response.should redirect_to(:action => :index, :controller => :settings)
+        end
+
+        it "uploads a local logo" do
+          uploaded_file = fixture_file_upload(Rails.root.join('spec/fixtures/images/example.png'), "image/png")
+          post(:change_logo,
+            {:source => :local, :file => uploaded_file})
+          File.file?(@image_path).should == true
+          response.should redirect_to(:action => :index, :controller => :settings)
+        end
+
+        it "fails uploading a non-image logo" do
+          uploaded_file = fixture_file_upload(Rails.root.join('spec/fixtures/videos/example.flv'), "video/x-flv")
+          post(:change_logo, { :source => :local, :file => uploaded_file })
+          flash[:failure].should_not == nil
+          File.file?(@image_path).should == false
+          response.should redirect_to(:action => :index, :controller => :settings)
+        end
+      end
+
+      describe :delete_logo do
+        it "really erases the logo" do
+          FileUtils.touch(@image_path)
+          get(:delete_logo)
+          flash[:success].should_not == nil
+          File.file?(@image_path).should == false
+          response.should redirect_to(:action => :index, :controller => :settings, :anchor => :Logo)
+        end
+      end
     end
 
     describe :themes do
       it "returns 200" do
         get :themes
         response.code.should eq("200")
-      end    
+      end
     end
-    
+
     describe :new_theme_install do
       it "returns 200" do
         get :new_theme_install
         response.code.should eq("200")
-      end    
-    end  
+      end
+    end
 
 		describe :install_theme do
 			before(:all) do
@@ -98,9 +99,9 @@ describe SettingsController do
 				response.should redirect_to(:action => :themes, :controller => :settings)
 				FileUtils.remove_dir(@theme_dir) if File.directory?(@theme_dir)
 			end
-			
+
 			it "installs new theme from a local file" do
-        uploaded_file = fixture_file_upload(Rails.root.join('spec/fixtures/themes/test-theme.zip'), "application/zip")       
+        uploaded_file = fixture_file_upload(Rails.root.join('spec/fixtures/themes/test-theme.zip'), "application/zip")
 				post(:install_theme, { :source => :local, :file => uploaded_file} )
 				flash[:success].should_not == nil
 				File.directory?(@theme_dir).should == true
@@ -108,14 +109,14 @@ describe SettingsController do
 				response.should redirect_to(:action => :themes, :controller => :settings)
 				FileUtils.remove_dir(@theme_dir) if File.directory?(@theme_dir)
 			end
-		end        
+		end
 
 		describe :delete_theme do
 			before(:each) do
 				@theme_dir = Rails.root.to_s + "/public/themes/test-theme"
 				FileUtils.cp_r(Rails.root.to_s + '/spec/fixtures/themes/test-theme', Rails.root.to_s + '/public/themes/')
 			end
-			
+
 			it "deletes an installed theme" do
 				get(:delete_theme, { :theme_name => "test-theme" })
 				flash[:success].should_not == nil
