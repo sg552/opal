fca784c88d93bc653f27d8b8b99a9bb90661d911
Siwei Shen <sg552sg552@gmail.com>
2012-02-03 21:57:23 +0800

    added images/js/css files from Xiaopan for solution3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,16 @@
 //= require rails
 //= require flowplayer-3.2.6.min
 //= require_tree .
+//= require xiaopan_menu_jquery1.3
+//= require xiaopan_new
+//= require photoshow
+
+function setTab(m,n){
+  var menu=document.getElementById("tab"+m).getElementsByTagName("li");
+  var showdiv=document.getElementById("tablist"+m).getElementsByTagName("div");
+  for(i=0;i<menu.length;i++)
+  {
+     menu[i].className=i==n?"now":"";
+     showdiv[i].style.display=i==n?"block":"none";
+  }
+}
